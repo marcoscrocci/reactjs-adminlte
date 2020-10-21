9a78bd71b1c48631acd508798d3222f105a49750
Marcos M. Crocci <marcoscrocci@gmail.com>
2020-10-21 20:49:17 -0300

    Finalizado a adaptação do AdminLTE 3 para o projeto Reactjs.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Menu from './components/Menu'
+
 
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <Content />
+            <Footer />
+        </div>
+    )
 }
 
 
