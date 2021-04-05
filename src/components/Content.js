37d32538cec23b847da6b1f53048788df1c89c1e
Marcos Crocci <croccim@ihsl.com>
2021-04-05 15:57:16 -0300

    Atualização

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import MaterialTable from 'material-table'
 
 export default function Content() {
     return (
         <div className="content-wrapper">
-
+            <MaterialTable
+                title="Action Overriding Preview"
+                columns={[
+                    { title: 'Name', field: 'name' },
+                    { title: 'Surname', field: 'surname' },
+                    { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                    {
+                        title: 'Birth Place',
+                        field: 'birthCity',
+                        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+                    },
+                ]}
+                data={[
+                    { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                    { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                ]}
+            />
         </div>
     )
 }
